48f04820ba789e73e10058414c78751f675fde3d
Brock T <brockthigpen@yahoo.com>
2020-01-17 16:46:19 -0500

    added queries to add employee

--- server.js
+++ server.js
@@ -66,7 +66,6 @@ const viewAllEmployees = () => {
 };
 const viewEmployeesByDepartment = () => {
     connection.query('SELECT * FROM department', (err, res) => {
-        console.log(res);
         if (err) throw err;
         inquirer.prompt({
             name: 'eByD',
@@ -79,9 +78,11 @@ const viewEmployeesByDepartment = () => {
                 query += 'LEFT JOIN employee m ON e.manager_id = m.id ';
                 query += 'INNER JOIN role r ON e.role_id = r.id ';
                 query += 'INNER JOIN department d ON r.department_id = d.id ';
-                query += 'WHERE(d.name =  ?) ORDER BY e.id';
+                query += 'WHERE ? ORDER BY e.id';
                 connection.query(query,
-                    choice.eByD,
+                    {
+                    name: choice.eByD
+                    },
                     (err, res) => {
                         if (err) throw err;
                         console.table(res);
@@ -92,7 +93,6 @@ const viewEmployeesByDepartment = () => {
 }
 const viewEmployeesByRole = () => {
     connection.query('SELECT title FROM role', (err, res) => {
-        console.log(res);
         let arr=[];
         res.forEach(i => arr.push(i.title));
         if (err) throw err;
@@ -107,9 +107,11 @@ const viewEmployeesByRole = () => {
                 query += 'LEFT JOIN employee m ON e.manager_id = m.id ';
                 query += 'INNER JOIN role r ON e.role_id = r.id ';
                 query += 'INNER JOIN department d ON r.department_id = d.id ';
-                query += 'WHERE(r.title =  ?) ORDER BY e.id';
+                query += 'WHERE ? ORDER BY e.id';
                 connection.query(query,
-                    choice.eByR,
+                    {
+                    title: choice.eByR
+                    },
                     (err, res) => {
                         if (err) throw err;
                         console.table(res);
@@ -135,5 +137,78 @@ const viewAllRoles = () =>{
     })
 }
 const addEmployee = () =>{
+    let roles=[];
+    let mgrs=['null'];
+    connection.query('SELECT title FROM role',
+    (err, res) => {
+        if (err) throw err;
+        res.forEach(i => roles.push(i.title));
+    });
+    connection.query('SELECT first_name, last_name FROM employee',
+    (err, res) => {
+        if (err) throw err;
+        res.forEach(i => {
+            let mgr = i.first_name.concat(' ', i.last_name)
+            mgrs.push(mgr)
+        })
+    });
+    inquirer.prompt([
+    {
+        name:'firstName',
+        type: 'input',
+        message: "What is the employee's first name?"
+    },
+    {
+        name:'lastName',
+        type: 'input',
+        message: "What is the emplyee's last name?"
+    },
+    {
+        name: 'role',
+        type: 'list',
+        message: "What is the employee's role?",
+        choices: roles
+    },
+    {
+        name: 'mgr',
+        type: 'list',
+        message: "Who is this employee's manager?",
+        choices: mgrs
+    }])
+    .then(ans => {
+        connection.query('SELECT id FROM role WHERE ?',
+        {
+        title: ans.role
+        }, 
+        (err, res) => {
+            if (err) throw err;
+            let roleId = res[0].id;
+            let mgr = ans.mgr.split(' ');
+            connection.query('SELECT id FROM employee WHERE ? AND ?',
+            [{
+                first_name: mgr[0]
+            },
+            {
+                last_name: mgr[1]
+            }],
+            (err, res) => {
+                if (err) throw err;
+                let mgrId = res[0].id;
 
+                connection.query('INSERT INTO employee SET ?',
+                {
+                    first_name: ans.firstName,
+                    last_name: ans.lastName,
+                    role_id: roleId,
+                    manager_id: mgrId
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    start();
+                })
+            })
+
+        })
+        
+    })
 }
