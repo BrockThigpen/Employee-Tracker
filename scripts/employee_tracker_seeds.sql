7e720eb0deb4f086f24b7cbb966fcbd6976fc52e
Brock T <brockthigpen@yahoo.com>
2020-01-18 16:44:51 -0500

    adjusted seed file

--- scripts/employee_tracker_seeds.sql
+++ scripts/employee_tracker_seeds.sql
@@ -2,11 +2,11 @@ USE emlployee_tracker_db;
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
     VALUES 
-        ('Brock', 'Thigpen', 1, 1);
+        ('Brock', 'Thigpen', 1, null);
 
 INSERT INTO role (title, salary, department_id)
     VALUES
-        ('student', 90000.00, 1);
+        ('student', 50000.00, 1);
 
 INSERT INTO department (name)
     VALUES
